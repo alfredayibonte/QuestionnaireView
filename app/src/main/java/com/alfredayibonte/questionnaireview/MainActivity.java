3eab3565c1dfeb2fc29ed193c5e9e15afc0f7c1d
Alfred <alfredayibonte@gmail.com>
2017-05-07 19:18:49 +0000

    add EditText example

--- app/src/main/java/com/alfredayibonte/questionnaireview/MainActivity.java
+++ app/src/main/java/com/alfredayibonte/questionnaireview/MainActivity.java
@@ -1,8 +1,14 @@
 package com.alfredayibonte.questionnaireview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+
 import com.alfredayibonte.questionnaireviewlib.QuestionnaireView;
 import com.alfredayibonte.questionnaireviewlib.adapters.CheckListAdapter;
 import com.alfredayibonte.questionnaireviewlib.adapters.RadioListAdapter;
@@ -12,7 +18,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         CheckListAdapter.OnCheckItemClickListener,
-        RadioListAdapter.OnRadioItemClickListener {
+        RadioListAdapter.OnRadioItemClickListener, TextView.OnEditorActionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +26,10 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         QuestionnaireView questionnaireView = (QuestionnaireView)findViewById(R.id.questionnaire);
         questionnaireView.setQuestion("What is the name of moses' father ?");
-        questionnaireView.setViewType(AnswerType.RADIO);
+        questionnaireView.setViewType(AnswerType.EDITTEXT);
         questionnaireView.addRadioItemListener(this);
         questionnaireView.addCheckItemListener(this);
+        questionnaireView.addOnEditorActionListener(this);
     }
 
     @Override
@@ -33,5 +40,11 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onRadioItemClick(List<Answer> answers) {
         Log.e("radio answers: ", answers.toString());
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+        return (i == EditorInfo.IME_ACTION_DONE);
     }
 }
